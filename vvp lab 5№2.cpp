e30c67928422c989997e49e89f96ad40834da350
Валерия <valeria103020@gmail.com>
2020-10-15 10:52:25 +0300

    Добавьте файлы проекта.

---
+++ vvp lab 5№2.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+#include<math.h>
+
+
+int main()
+{
+	int A, B, C;
+	int x, y;
+	printf("A: ");
+	scanf_s("%d", &A);
+	printf("B: ");
+	scanf_s("%d", &B);
+	printf("C: ");
+	scanf_s("%d", &C);
+	printf("%d",abs(C-A)+abs(C-B));
+	return 0;
+}
